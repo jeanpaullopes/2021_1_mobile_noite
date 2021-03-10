e4078aa36b618c1d8bd00e78104cba7f8d9f85b0
Prof Jean Paul <jean.paul@uniritter.edu.br>
2021-03-10 14:55:38 -0300

    Nosso primeiro commit

---
+++ app/src/main/java/br/edu/uniritter/mobile/nossaprimeiraappnoite/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package br.edu.uniritter.mobile.nossaprimeiraappnoite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
